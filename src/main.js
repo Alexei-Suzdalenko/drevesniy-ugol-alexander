83f4f08db652daa02a97f72e7bf5b74fad30c21b
Alexei-Suzdalenko <alexei.saron@gmail.com>
2021-10-01 23:35:22 +0200

    first commit jui

---
+++ src/main.js
@@ -0,0 +1,43 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+// import './registerServiceWorker'
+import router from './router';
+import store from './store';
+
+import firebase from 'firebase';
+const firebaseConfig = {
+  apiKey: "AIzaSyAPstPLbudYMnrH71umjP_vZNjGJZ6ENlI",
+  authDomain: "drevesniy-ugol.firebaseapp.com",
+  databaseURL: "https://drevesniy-ugol-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "drevesniy-ugol",
+  storageBucket: "drevesniy-ugol.appspot.com",
+  messagingSenderId: "872882928313",
+  appId: "1:872882928313:web:b16d3d82247e21f55af968",
+  measurementId: "G-K7WE4YYZ1X"
+};
+
+firebase.initializeApp(firebaseConfig);
+createApp(App).use(store).use(router).mount('#app');
+
+// drevesniy.ugol@gmail.com svoboda2019svoboda2019         // firebase alexei.saron@gmail.com
+// https://drevesniy-ugol.000webhostapp.com/
+// https://drevesniy-ugol.web.app
+// google drevesniy.ugol@gmail.com  svoboda2019svoboda2019 // 000webhost
+// host: files.000webhost.com, username: drevesniy-ugol, password: ...
+
+// curs http://ip-api.com/json/?fields=status,message,countryCode
+// https://youtu.be/mpK_MYb38zs?list=PLoonZ8wII66iUm84o7nadL-oqINzBLk5g
+// npm install firebase
+// deer crunch mesh position always chicken cruel hurt pulse defy plastic version
+// https://www.ekko-wp.com/moving/
+// feed antique light feed axis pact armed cereal limit close glow sunny
+// https://moling.dan-fisher.com/index.html
+// https://html.modernwebtemplates.com/moveco/
+// https://html.modernwebtemplates.com/mover/index.html
+// https://templatebundle.net/templates/mover-and-packers-html-template/
+// other theme https://themewagon.com/
+// https://technext.github.io/yavin/index.html#header 
+// https://technext.github.io/nubis/index.html
+// vue js template
+
+
---
+++ src/storage/urls.js
@@ -0,0 +1,12 @@
+let urls = [
+'Mudanzas/Abanillas',
+'Vaciados-de-pisos/Abanillas',
+'Reformas/Abanillas',
+'Pintura/Abanillas',
+'Recogida-muebles/Abanillas',
+'Limpiezas/Abanillas',
+'Mudanzas/Abaño',
+
+];
+
+export default { urls }
